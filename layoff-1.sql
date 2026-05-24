13928486ace0a5e4fa0a878148217d14f7c1cca8
bwbeas <bellbuuuwork6@gmail.com>
2026-05-24 21:58:46 +0530

    removing null 1

--- layoff-1.sql
+++ layoff-1.sql
@@ -51,4 +51,45 @@ WHERE row_num>1;
 
 
 -- STANDARDIZING DATA
+SELECT company, TRIM(company) FROM layoffs_staging2;
+
+UPDATE layoffs_staging2 SET company= TRIM(company);
+
+SELECT * FROM layoffs_staging2 
+WHERE industry LIKE 'Crypto%';
+
+UPDATE layoffs_staging2 
+SET industry= 'Crypto'
+WHERE industry LIKE 'Crypto%';
+
+UPDATE layoffs_staging2 
+SET country= TRIM(TRAILING '.' FROM country)
+WHERE country LIKE 'United States%';
+
+-- changing date format to proper time
+SELECT `date`, str_to_date(`date`,'%m/%d/%Y')
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET `date`=str_to_date(`date`,'%m/%d/%Y');
+
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN `date` DATE;
+
+
+-- coming to null values
+SELECT * FROM layoffs_staging2
+WHERE total_laid_off IS NULL AND percentage_laid_off IS NULL;
+
+SELECT * FROM layoffs_staging2 
+WHERE industry IS NULL OR industry = '' ;
+
+SELECT * FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+ON t1.company= t2.company AND t1.location=t2.location
+WHERE (t1.industry IS NULL OR t1.industry ='') AND t2.industry IS NOT NULL;
+
+
+
+
 
