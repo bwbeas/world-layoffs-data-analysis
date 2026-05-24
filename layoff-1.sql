49bbba461af089fa7fa98c7d42d1b2192eeed50c
bwbeas <bellbuuuwork6@gmail.com>
2026-05-24 21:17:59 +0530

    duplicate data deletion

--- layoff-1.sql
+++ layoff-1.sql
@@ -13,6 +13,7 @@ SELECT * FROM layoffs_staging;
 INSERT layoffs_staging
 SELECT * FROM layoffs;
 
+-- REMOVING DUPLICATE
 
 WITH duplicate_cte AS(
 SELECT * , ROW_NUMBER() OVER(
@@ -31,7 +32,23 @@ CREATE TABLE `layoffs_staging2` (
   `date` text,
   `stage` text,
   `country` text,
-  `funds_raised_millions` int DEFAULT NULL
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` int
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+SELECT * FROM layoffs_staging2;
+
+INSERT INTO layoffs_staging2
+SELECT * , ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, total_laid_off, percentage_laid_off,`date`, stage, country, funds_raised_millions) AS row_num
+FROM layoffs_staging;
+
+SELECT * FROM layoffs_staging2
+WHERE row_num>1;
+
+DELETE FROM layoffs_staging2
+WHERE row_num>1;
+
+
+-- STANDARDIZING DATA
 
