52afea3a264d6b4e808fdc063f96f82391e59936
bwbeas <bellbuuuwork6@gmail.com>
2026-05-26 16:25:20 +0530

    final commit for project

--- layoff-2.sql
+++ layoff-2.sql
@@ -1,5 +1,7 @@
 -- Exploratory data analysis
+
 SELECT * FROM layoffs_staging2;
+
 SELECT MAX(total_laid_off), MAX(percentage_laid_off)
 FROM layoffs_staging2;
 
@@ -21,3 +23,37 @@ FROM layoffs_staging2
 WHERE SUBSTRING(`date`,1,7) IS NOT NULL
 GROUP BY `month`
 ORDER BY 1 ASC;
+
+WITH Rolling_Total AS
+(
+SELECT SUBSTRING(`date`,1,7) AS `month`, SUM(total_laid_off) AS total_off
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `month`
+)
+SELECT `month`, total_off, SUM(total_off) OVER(ORDER BY `month`) AS rolling_total
+FROM Rolling_Total;
+
+
+
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+ORDER BY company ASC;
+
+
+
+With Company_Year (company, years, total_laid_off) AS
+(
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+), Company_Year_Rank AS
+(
+SELECT *, 
+DENSE_RANK() OVER(PARTITION BY years ORDER BY total_laid_off DESC) AS Ranking
+FROM Company_Year
+WHERE years IS NOT NULL
+)
+SELECT * FROM Company_Year_Rank
+WHERE Ranking<=5;
